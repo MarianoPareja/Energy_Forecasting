d0c53932572ff0d59de26ec0b3b1145e4a8e9890
Mariano Pareja <mariano@Marianos-MacBook-Pro.local>
2024-04-20 14:01:00 -0400

    Update: app-api internal docker url updated

--- src/app-frontend/frontend/settings.py
+++ src/app-frontend/frontend/settings.py
@@ -1,4 +1,4 @@
 from yarl import URL
 
 TITLE = "Energy Consumption Forecasting"
-API_URL = URL("http://172.20.0.1:8001/api/v1")
+API_URL = URL("http://172.20.0.6:8001/api/v1")
--- src/app-monitoring/monitoring/settings.py
+++ src/app-monitoring/monitoring/settings.py
@@ -1,4 +1,4 @@
 from yarl import URL
 
 TITLE = "Monitoring | Energy Consumption"
-API_URL = URL("http://172.20.0.1:8001/api/v1")
+API_URL = URL("http://172.20.0.6:8001/api/v1")
